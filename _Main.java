603df646ccdb2d115a2943ef247cfbc0fa3020e6
swaralichawdw <swaralichawde@gmail.com>
2025-04-20 15:45:44 +0530

    Add files via upload

---
+++ _Main.java
@@ -0,0 +1,15 @@
+class Animal {
+    void sound() {
+        
+        System.out.println("Animal makes a sound");
+    }
+}
+class Dog extends Animal {
+    void sound() {
+        System.out.println("Dog barks");
+    }
+    public static void main(String[] args) {
+        Dog d = new Dog();
+        d.sound();
+    }
+}
